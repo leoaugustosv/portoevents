267e162407bfc318117ff5ded44b8f44727344bb
leoaugustosv <kleeniietv@gmail.com>
2024-05-25 21:43:31 -0300

    tratar possibilidades de listagens de evento vazias

--- src/com/linsysdev/portoevents/eventos/EventosUtil.java
+++ src/com/linsysdev/portoevents/eventos/EventosUtil.java
@@ -26,36 +26,43 @@ public class EventosUtil extends Eventos {
 
                 }
 
-                listaEventos.sort((String[] o1, String[] o2) -> o1[9].compareTo(o2[9]));
+                if (listaEventos.isEmpty()) {
+                    System.out.println(">> INFO: Não constam eventos futuros em sistema.");
+                    System.out.println();
+                } else {
 
-                System.out.println("========= PRÓXIMOS EVENTOS =========");
-                System.out.println("Aqui estão os próximos eventos, do mais próximo ao mais distante: ");
-                System.out.println();
-                int numeroEvento = 1;
+                    listaEventos.sort((String[] o1, String[] o2) -> o1[9].compareTo(o2[9]));
 
-                for (String[] e : listaEventos) {
-                    System.out.println("EVENTO " + (numeroEvento));
-                    System.out.println("==================================");
-                    System.out.println("> NOME: " + e[0]);
-                    System.out.println(
-                            "> ENDEREÇO: " + e[1] + ", " + e[2] + " (" + e[3] + ") - " + e[8] + " - " + e[4] + " - "
-                                    + e[5] + ", " + e[6]);
+                    System.out.println("========= PRÓXIMOS EVENTOS =========");
+                    System.out.println("Aqui estão os próximos eventos, do mais próximo ao mais distante: ");
                     System.out.println();
-                    System.out.println("> HORA DE INÍCIO: " + e[9]);
-                    System.out.println(
-                            "> HORA DO TÉRMINO: " + LocalDateTime.parse(e[9]).plusMinutes(Integer.parseInt(e[10])));
+                    int numeroEvento = 1;
+
+                    for (String[] e : listaEventos) {
+                        System.out.println("EVENTO " + (numeroEvento));
+                        System.out.println("==================================");
+                        System.out.println("> NOME: " + e[0]);
+                        System.out.println(
+                                "> ENDEREÇO: " + e[1] + ", " + e[2] + " (" + e[3] + ") - " + e[8] + " - " + e[4] + " - "
+                                        + e[5] + ", " + e[6]);
+                        System.out.println();
+                        System.out.println("> HORA DE INÍCIO: " + e[9]);
+                        System.out.println(
+                                "> HORA DO TÉRMINO: " + LocalDateTime.parse(e[9]).plusMinutes(Integer.parseInt(e[10])));
+                        System.out.println();
+                        System.out.println("> CATEGORIA: " + e[8]);
+                        System.out.println("> DESCRIÇÃO: " + e[11]);
+                        System.out.println("==================================");
+                        System.out.println();
+                        numeroEvento++;
+                    }
+
+                    System.out.println(">> INFO: Eventos futuros: " + contagemEventos + ".");
                     System.out.println();
-                    System.out.println("> CATEGORIA: " + e[8]);
-                    System.out.println("> DESCRIÇÃO: " + e[11]);
-                    System.out.println("==================================");
-                    System.out.println();
-                    numeroEvento++;
                 }
-
-                System.out.println(">> INFO: Eventos futuros: " + contagemEventos + ".");
-                System.out.println();
             } else {
                 System.out.println(">> INFO: Não existem eventos cadastrados em sistema.");
+                System.out.println();
             }
 
         } catch (IOException e) {
@@ -81,36 +88,43 @@ public class EventosUtil extends Eventos {
 
                 }
 
-                listaEventos.sort((String[] o1, String[] o2) -> o2[9].compareTo(o1[9]));
+                if (listaEventos.isEmpty()) {
+                    System.out.println(">> INFO: Não existem eventos já ocorridos até o momento.");
+                    System.out.println();
+                } else {
+                    listaEventos.sort((String[] o1, String[] o2) -> o2[9].compareTo(o1[9]));
 
-                System.out.println("========= EVENTOS PASSADOS =========");
-                System.out.println("Aqui estão os próximos eventos, do mais próximo ao mais distante: ");
-                System.out.println();
-                int numeroEvento = 1;
+                    System.out.println("========= EVENTOS PASSADOS =========");
+                    System.out.println("Aqui estão os próximos eventos, do mais próximo ao mais distante: ");
+                    System.out.println();
+                    int numeroEvento = 1;
 
-                for (String[] e : listaEventos) {
-                    System.out.println("EVENTO " + (numeroEvento));
-                    System.out.println("==================================");
-                    System.out.println("> NOME: " + e[0]);
-                    System.out.println(
-                            "> ENDEREÇO: " + e[1] + ", " + e[2] + " (" + e[3] + ") - " + e[8] + " - " + e[4] + " - "
-                                    + e[5] + ", " + e[6]);
+                    for (String[] e : listaEventos) {
+                        System.out.println("EVENTO " + (numeroEvento));
+                        System.out.println("==================================");
+                        System.out.println("> NOME: " + e[0]);
+                        System.out.println(
+                                "> ENDEREÇO: " + e[1] + ", " + e[2] + " (" + e[3] + ") - " + e[8] + " - " + e[4] + " - "
+                                        + e[5] + ", " + e[6]);
+                        System.out.println();
+                        System.out.println("> HORA DE INÍCIO: " + e[9]);
+                        System.out.println(
+                                "> HORA DO TÉRMINO: " + LocalDateTime.parse(e[9]).plusMinutes(Integer.parseInt(e[10])));
+                        System.out.println();
+                        System.out.println("> CATEGORIA: " + e[8]);
+                        System.out.println("> DESCRIÇÃO: " + e[11]);
+                        System.out.println("==================================");
+                        System.out.println();
+                        numeroEvento++;
+                    }
+
+                    System.out.println(">> INFO: Eventos passados: " + contagemEventos + ".");
                     System.out.println();
-                    System.out.println("> HORA DE INÍCIO: " + e[9]);
-                    System.out.println(
-                            "> HORA DO TÉRMINO: " + LocalDateTime.parse(e[9]).plusMinutes(Integer.parseInt(e[10])));
-                    System.out.println();
-                    System.out.println("> CATEGORIA: " + e[8]);
-                    System.out.println("> DESCRIÇÃO: " + e[11]);
-                    System.out.println("==================================");
-                    System.out.println();
-                    numeroEvento++;
                 }
 
-                System.out.println(">> INFO: Eventos passados: " + contagemEventos + ".");
-                System.out.println();
             } else {
                 System.out.println(">> INFO: Não existem eventos cadastrados em sistema.");
+                System.out.println();
             }
 
         } catch (IOException e) {
