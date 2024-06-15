feaf1482795728e82bdbccf272f87ca8e506cd47
Leonardo Silveira <kleeniietv@gmail.com>
2024-06-14 21:48:46 -0300

    fix: remover unused imports

--- src/com/linsysdev/portoevents/eventos/EventosUtil.java
+++ src/com/linsysdev/portoevents/eventos/EventosUtil.java
@@ -1,7 +1,6 @@
 package com.linsysdev.portoevents.eventos;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
